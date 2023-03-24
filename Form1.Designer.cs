1a4c6580bbccbef1aa9329f596197ad31285ed8f
paulacortesr18 <125370008+paulacortesr18@users.noreply.github.com>
2023-03-24 11:40:58 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace APP_HAS
+{
+    partial class ValidaciónDatos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ValidaciónDatos
+            // 
+            this.ClientSize = new System.Drawing.Size(383, 311);
+            this.Name = "ValidaciónDatos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombres;
+        private System.Windows.Forms.Label lblApellidos;
+        private System.Windows.Forms.Label lblTipodeId;
+        private System.Windows.Forms.Label lblGenero;
+        private System.Windows.Forms.Label lblEstadoCivil;
+        private System.Windows.Forms.Label lblCiudadResidencia;
+        private System.Windows.Forms.Label lblDireccióndeResidencia;
+        private System.Windows.Forms.Label lblTelefonodeContacto;
+        private System.Windows.Forms.Label lblOcupación;
+        private System.Windows.Forms.Label lblNiveldeEscolaridad;
+        private System.Windows.Forms.Label lblEPS;
+        private System.Windows.Forms.Label lblRegimen;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblContactoEmergencia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.ComboBox comboBox7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace APP_HAS
+{ 
+    if (System is null)
+        {
+            throw private new ArgumentNullException(NameofNewStruct);
+
+    internal struct NewStruct
+    {
+        private SystemPens item1;
+        private object item2;
+
+        public SystemPens Item1 { get => item1; set => item1 = value; }
+        public object Item2 { get => item2; set => item2 = value; }
+
+        public NewStruct(SystemPens item1, object item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is NewStruct other &&
+                   EqualityComparer<SystemPens>.Default.Equals(Item1, other.Item1) &&
+                   EqualityComparer<object>.Default.Equals(Item2, other.Item2);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030903623;
+            hashCode = hashCode * -1521134295 + EqualityComparer<SystemPens>.Default.GetHashCode(Item1);
+            hashCode = hashCode * -1521134295 + EqualityComparer<object>.Default.GetHashCode(Item2);
+            return hashCode;
+        }
+
+        public void Deconstruct(out SystemPens item1, out object item2)
+        {
+            item1 = Item1;
+            item2 = Item2;
+        }
+
+        public static implicit operator (SystemPens, object)(NewStruct value)
+        {
+            return (value.Item1, value.Item2);
+        }
+
+        public static implicit operator NewStruct((SystemPens, object) value)
+        {
+            return new NewStruct(value.Item1, value.Item2);
+        }
+    }
+}
+
+    public partial class ValidaciónDatos : Form
+{
+    public ValidaciónDatos()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+}
+
